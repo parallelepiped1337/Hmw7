01bf88c0e135a71c7df905e28d332bb6fe024f0b
parallelepiped1337 <andrejbuiukli@gmail.com>
2024-10-18 17:39:53 +0300

    Update Main.java

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,100 +1,121 @@
 import java.util.Random;
 
-import java.util.Scanner;
-
 public class Main {
-
+    
     public static void main(String[] args) {
-
-        int[] array = new int[15];
+        int[][] matrix = new int[4][4];
         Random random = new Random();
-
-
-        for (int i = 0; i < array.length; i++) {
-            array[i] = random.nextInt(100) + 1;
-
+        
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                matrix[i][j] = random.nextInt(50) + 1;
+                
+            }
+            
         }
 
+        System.out.println("Матриця 4x4:");
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.printf("%3d ", matrix[i][j]);
+                
+            }
+            
+            System.out.println();
+            
+        }
 
-        System.out.print("Початковий вигляд масиву: ");
-        printArray(array);
+        int sumEvenRows = 0;
+        int sumOddRows = 0;
 
-        insertionSort(array);
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (i % 2 == 0) {
+                    sumEvenRows += matrix[i][j];
+                } else {
+                    sumOddRows += matrix[i][j];
+                    
+                }
+                
+            }
+            
+        }
 
-        System.out.print("Відсортований масив: ");
-        printArray(array);
+        long productEvenCols = 1;
+        long productOddCols = 1;
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Введіть число для пошуку: ");
-        int target = scanner.nextInt();
-        int index = binarySearch(array, target);
+        for (int j = 0; j < 4; j++) {
+            for (int i = 0; i < 4; i++) {
+                if (j % 2 == 0) {
+                    productEvenCols *= matrix[i][j];
+                } else {
+                    productOddCols *= matrix[i][j];
+                    
+                }
+                
+            }
+            
+        }
 
-        if (index != -1) {
-            System.out.println("Індекс числа " + target + " у відсортованому масиві: " + index);
+        System.out.println("Сума елементів у парних рядках (рядок 0, 2): " + sumEvenRows);
+        System.out.println("Сума елементів у непарних рядках (рядок 1, 3): " + sumOddRows);
+        System.out.println("Добуток елементів у парних стовпцях (стовпець 0, 2): " + productEvenCols);
+        System.out.println("Добуток елементів у непарних стовпцях (стовпець 1, 3): " + productOddCols);
+
+        if (isMagicSquare(matrix)) {
+            System.out.println("Матриця є магічним квадратом.");
         } else {
-            System.out.println("Число " + target + " не знайдено у масиві.");
-
+            System.out.println("Матриця не є магічним квадратом.");
+            
         }
-
+        
     }
 
-    private static void printArray(int[] array) {
+    public static boolean isMagicSquare(int[][] matrix) {
+        int n = matrix.length;
+        int magicSum = 0;
 
-        System.out.print("[");
-        for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i]);
-            if (i < array.length - 1) {
-                System.out.print(", ");
-
-            }
-
+        for (int j = 0; j < n; j++) {
+            magicSum += matrix[0][j];
+            
         }
 
-        System.out.println("]");
-
-    }
-
-    private static void insertionSort(int[] array) {
-        for (int i = 1; i < array.length; i++) {
-            int key = array[i];
-            int j = i - 1;
-
-            while (j >= 0 && array[j] > key) {
-                array[j + 1] = array[j];
-                j--;
-
+        for (int i = 1; i < n; i++) {
+            int rowSum = 0;
+            for (int j = 0; j < n; j++) {
+                rowSum += matrix[i][j];
             }
-
-            array[j + 1] = key;
-
+            if (rowSum != magicSum) {
+                return false;
+                
+            }
+            
         }
 
-    }
-
-
-    private static int binarySearch(int[] array, int target) {
-        int left = 0;
-        int right = array.length - 1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (array[mid] == target) {
-                return mid;
-
+        for (int j = 0; j < n; j++) {
+            int colSum = 0;
+            for (int i = 0; i < n; i++) {
+                colSum += matrix[i][j];
+                
             }
-
-            if (array[mid] < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-
+            
+            if (colSum != magicSum) {
+                return false;
+                
             }
-
+            
         }
 
-        return -1;
-
+        int diagonalSum1 = 0;
+        int diagonalSum2 = 0;
+        for (int i = 0; i < n; i++) {
+            diagonalSum1 += matrix[i][i];
+            diagonalSum2 += matrix[i][n - 1 - i];
+            
+        }
+    
+        return diagonalSum1 == magicSum && diagonalSum2 == magicSum;
+        
     }
-
+    
 }
