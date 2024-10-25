6ce7b92020a922fc76d9a80d739aca458fb7163b
parallelepiped1337 <andrejbuiukli@gmail.com>
2024-10-25 18:53:57 +0300

    Update Main.java

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,5 +1,5 @@
-class Person {
-    
+public class Person {
+
     private String name;
     private int age;
     private String profession;
@@ -30,14 +30,17 @@ public class Main {
         Person person2 = new Person("Mary", 25, "Вчитель");
         Person person3 = new Person("Bob", 35, "Лікар");
 
-
+        System.out.println("Інформація про осіб:");
         person1.displayInfo();
         person2.displayInfo();
         person3.displayInfo();
 
         person1.setProfession("Архітектор");
-
         System.out.println("\n(Після оновлення професії)");
+        person1.displayInfo();
+
+        person1.setProfession("Дизайнер");
+        System.out.println("\n(Після другого оновлення професії)");
         person1.displayInfo();
         
     }
