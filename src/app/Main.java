f4d36fb4e913e60e7c0ad61c9447e028bf44fd06
parallelepiped1337 <andrejbuiukli@gmail.com>
2024-10-25 15:25:23 +0300

    Update Main.java

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,85 +1,44 @@
-import java.util.Scanner;
+class Person {
+    
+    private String name;
+    private int age;
+    private String profession;
+
+    public Person(String name, int age, String profession) {
+        this.name = name;
+        this.age = age;
+        this.profession = profession;
+        
+    }
+
+    public void setProfession(String profession) {
+        this.profession = profession;
+        
+    }
+
+    public void displayInfo() {
+        System.out.println("Ім'я: " + name + ", Вік: " + age + ", Професія: " + profession);
+        
+    }
+    
+}
 
 public class Main {
-
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        System.out.print("Введіть ціле число: ");
-        int number = scanner.nextInt();
-        printSquare(number);
-        
-        System.out.print("Введіть радіус: ");
-        double radius = scanner.nextDouble();
-        System.out.print("Введіть висоту: ");
-        double height = scanner.nextDouble();
-        double volume = calculateCylinderVolume(radius, height);
-        System.out.println("Об'єм циліндра з радіусом " + radius + " і висотою " + height + " дорівнює " + volume);
-        
-        int[] array = {10, 20, 30, 40, 50};
-        int sum = calculateArraySum(array);
-        System.out.println("Масив чисел: [10, 20, 30, 40, 50]");
-        System.out.println("Сума всіх елементів масиву дорівнює " + sum);
-        
-        System.out.print("Введіть рядок: ");
-        scanner.nextLine();
-        String text = scanner.nextLine();
-        String reversedText = reverseString(text);
-        System.out.println("Рядок в зворотньому порядку: " + reversedText);
-        
-        System.out.print("Введіть a: ");
-        int a = scanner.nextInt();
-        System.out.print("Введіть b: ");
-        int b = scanner.nextInt();
-        int powerResult = power(a, b);
-        System.out.println("Результат " + a + "^" + b + " дорівнює " + powerResult);
-        
-        System.out.print("Введіть ціле число n: ");
-        int n = scanner.nextInt();
-        System.out.print("Введіть текстовий рядок: ");
-        scanner.nextLine();
-        String message = scanner.nextLine();
-        printTextMultipleTimes(n, message);
-        
-    }
 
-    public static void printSquare(int number) {
-        int square = number * number;
-        System.out.println("Квадрат числа " + number + " дорівнює " + square + ".");
-        
-    }
+        Person person1 = new Person("John", 30, "Інженер");
+        Person person2 = new Person("Mary", 25, "Вчитель");
+        Person person3 = new Person("Bob", 35, "Лікар");
 
-    public static double calculateCylinderVolume(double radius, double height) {
-        return Math.PI * radius * radius * height;
-        
-    }
 
-    public static int calculateArraySum(int[] array) {
-        int sum = 0;
-        for (int num : array) {
-            sum += num;
-            
-        }
-        
-        return sum;
-        
-    }
+        person1.displayInfo();
+        person2.displayInfo();
+        person3.displayInfo();
 
-    public static String reverseString(String text) {
-        return new StringBuilder(text).reverse().toString();
-        
-    }
+        person1.setProfession("Архітектор");
 
-    public static int power(int a, int b) {
-        return (int) Math.pow(a, b);
-        
-    }
-
-    public static void printTextMultipleTimes(int n, String text) {
-        for (int i = 0; i < n; i++) {
-            System.out.println(text);
-            
-        }
+        System.out.println("\n(Після оновлення професії)");
+        person1.displayInfo();
         
     }
     
